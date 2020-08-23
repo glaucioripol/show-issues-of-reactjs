0723f7030c4a0925cf7aa690fc165f148cf82948
Glaucio Ricardo <glaucio.ricardo.jun@accenture.com>
2020-08-23 18:31:40 -0300

    chore: add semantic ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
+import 'semantic-ui-css/semantic.min.css'
+
 import { App } from './App'
 import * as serviceWorker from './serviceWorker'
 
