a45b1d91b1b20834632940a121a2cde36673b80d
Glaucio Ricardo <glaucio.ricardo.jun@accenture.com>
2020-08-24 12:54:34 -0300

    refactor: componentize items in home page

--- src/pages/home/homeContent/index.js
+++ src/pages/home/homeContent/index.js
@@ -1,75 +1,16 @@
 import React from 'react'
 
-import { Container, Form } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 import { MyTable } from '../../../components'
+import { Filters } from './filters'
 
 import './home.css'
-
-const countryOptions = [
-  { key: 'all', value: 'all', text: 'all' },
-  { key: 'open', value: 'open', text: 'open' },
-  { key: 'closed', value: 'closed', text: 'closed' }
-]
-
-const quantityOption = [
-  { key: '10', value: '10', text: '10' },
-  { key: '25', value: '25', text: '25' },
-  { key: '50', value: '50', text: '50' },
-  { key: '100', value: '100', text: '100' }
-]
 
 export function HomeContent() {
   return (
     <Container fluid>
 
-      <section>
-        <h2>Filters</h2>
-
-        <div style={{ margin: '0 0 2rem' }}>
-          <Form onSubmit={() => { }}>
-            <Form.Group widths='equal'>
-
-              <Form.Field>
-                <input
-                  type="date"
-                  name="since"
-                  id="since"
-                />
-              </Form.Field>
-
-              <Form.Dropdown
-                selection
-                clearable
-                id="state"
-                placeholder='filter from state'
-                options={countryOptions}
-              />
-
-              <Form.Dropdown
-                selection
-                clearable
-                id="per_page"
-                placeholder='quantity for page'
-                options={quantityOption}
-              />
-
-            </Form.Group>
-
-            <Form.Button
-              circular
-              fluid
-              color="instagram"
-              loading
-              size="big"
-              disabled={false}
-            >
-                Filter
-            </Form.Button>
-          </Form>
-
-        </div>
-
-      </section>
+      <Filters/>
 
       <main className='container__table'>
         <MyTable />
