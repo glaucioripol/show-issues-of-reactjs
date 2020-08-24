7ecdbf8dea44179d57ad99ceefa30484844b00f5
Glaucio Ricardo <glaucio.ricardo.jun@accenture.com>
2020-08-24 17:45:22 -0300

    feat: add mock response endpoint

--- src/services/api/api.spec.js
+++ src/services/api/api.spec.js
@@ -5,19 +5,31 @@ import { retrieveIssues } from '.'
 import mockResponse from '../../tests/mocks/issuesApiResponse.json'
 
 describe('test my request', () => {
-  beforeAll(async() => {
+  beforeAll(() => {
     nock.cleanAll()
   })
-  beforeEach(async() => {
+  beforeEach(() => {
     nock.cleanAll()
   })
 
-  it('test nock', async () => {
+  it('Success request', async () => {
     nock('https://api.github.com')
       .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
       .get('/repos/facebook/react/issues?per_page=10')
       .reply(200, mockResponse)
 
-    expect(await retrieveIssues()).toEqual(mockResponse)
+    const response = await retrieveIssues()
+
+    expect(await response.json()).toEqual(mockResponse)
+  })
+
+  it('failure request', async () => {
+    nock('https://api.github.com')
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/repos/facebook/react/issues?per_page=10')
+      .reply(404)
+
+    const hasFailure = (await retrieveIssues()).ok
+    expect(hasFailure).toBeFalsy()
   })
 })
